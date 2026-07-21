db81a244c59df42480541c40bde7797d83b5797c
jeongsuji <sujijeong11@gmail.com>
2026-07-21 09:26:46 +0900

    findIndexById로 중복 제거

--- src/App.java
+++ src/App.java
@@ -1,4 +1,124 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
+
+    Scanner sc = new Scanner(System.in);
+    int lastId = 0; // 가장 최근 생성된 명언 번호. 번호 관리
+//    int lastIndex = 0; // 가장 최근 생성된 명언의 배열 인덱스. 배열 관리
+    ArrayList<WiseSaying>  wiseSayings = new ArrayList<>();
+
+    public void run() {
+
+        System.out.println("== 명언 앱 ==");
+
+        String cmd = "";
+        while (!cmd.equals("종료")) {
+            System.out.print("명령) ");
+            cmd = sc.nextLine();
+
+            if (cmd.equals("등록")) {
+                actionWrite(); // mac : option + enter, win : alt + enter
+            }
+            else if (cmd.equals("목록")) {
+                actionList();
+            }
+            else if (cmd.startsWith("삭제")) {
+                actionDelete(cmd);
+            }
+            else if (cmd.startsWith("수정")) {
+                actionModify(cmd);
+
+            }
+        }
+    }
+
+    public int findIndexById(int id){
+        int targetIdx = -1;
+
+        for(int i = 0; i < wiseSayings.size(); i++) {
+            WiseSaying w1 = wiseSayings.get(i);
+            if(w1.getId() == id) {
+                targetIdx = i;
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void actionModify(String cmd) {
+        String strId = cmd.substring(cmd.indexOf("=") + 1).trim();
+        int id = Integer.parseInt(strId);
+
+        //변하는 부분을 변하지 않는 부분에서 분리해라
+        int targetIdx = findIndexById(id);
+
+        if(targetIdx == -1){
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
+
+        //수정 로직
+        WiseSaying wiseSaying = wiseSayings.get(targetIdx);
+
+        System.out.println("명언(기존): %s".formatted(wiseSaying.getContent()) );
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+        wiseSaying.setContent(content);
+
+        System.out.println("작가(기존): %s".formatted(wiseSaying.getContent()) );
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+        wiseSaying.setAuthor(author);
+
+    }
+
+    private void actionDelete(String cmd) {
+
+        // 삭제?id=4 에서 1만 가져오는 법
+        // =까지 자르고, 그 뒤에 있는 1을 가져오기
+
+
+        String strId = cmd.substring(cmd.indexOf("=") + 1).trim();
+        int id = Integer.parseInt(strId);
+
+        int targetIdx = findIndexById(id);
+
+        if(targetIdx == -1){
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
+
+        wiseSayings.remove(targetIdx);
+    }
+
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+            WiseSaying target = wiseSayings.get(i);
+            System.out.println("%d / %s / %s.".formatted(target.getId(), target.getAuthor(), target.getContent()));
+        }
+    }
+
+    private WiseSaying write(String content, String author) {
+        ++lastId;
+        WiseSaying w1 = new WiseSaying(lastId, content, author);
+        wiseSayings.add(w1);
+//        ++lastIndex;
+
+        return w1;
+    }
+
+    private void actionWrite() {
+
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        WiseSaying wiseSaying = write(content, author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
+    }
 }
--- src/WiseSaying.java
+++ src/WiseSaying.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
 
+//Main.java에 두면? -> 이 클래스는 Main에만 관련이 있다는 뜻 => 클래스 의미 축소
 public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
