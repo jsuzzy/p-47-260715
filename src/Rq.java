ad9eaf6a8e3636327ba219424083d757cc5061bf
jeongsuji <sujijeong11@gmail.com>
2026-07-21 11:17:36 +0900

    Rq 클래스 getParam 구현

--- src/Rq.java
+++ src/Rq.java
@@ -1,13 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     private String cmd;
+    Map<String, String> paramMap = new HashMap<>();
 
     public Rq(String cmd) {
         this.cmd = cmd;
+
+        //키, 밸류 저장 전용 자료구조 -> map
+        //목록?keywordType=content&keyword=과거
+        String[] cmdBits = cmd.split("\\?"); //["목록", "keywordType=content&keyword=과거"]
+
+        String params = cmdBits[1];
+        String[] paramBits = params.split("&"); //["keywordType=content","keyword=과거"]
+
+        for(String param : paramBits){
+           String[] keyValue =  param.split("=");
+           paramMap.put(keyValue[0], keyValue[1]);
+        }
     }
 
     public String getActionName() {
         String[] cmdBits = cmd.split("\\?"); //?기호를 기준으로 잘라줌 ["삭제", "id=1"]
 
         return cmdBits[0];
+    }
+
+    public String getParam(String key, String defaultValue) {
+       return paramMap.get(key);
     }
 }
--- src/RqTest.java
+++ src/RqTest.java
@@ -1,12 +1,32 @@
 public class RqTest {
 
     public static void main(String[] args) {
+        testGetParam();
+    }
+
+    public static void testActionName(){
         Rq rq1 = new Rq("삭제?id=1");
-        rq1.getActionName(); //삭제
+        String action = rq1.getActionName(); //삭제
 
 
         Rq rq2 = new Rq("수정?id=1");
 
         rq2.getActionName(); //수정
     }
+
+    public static void testGetParam(){
+        Rq rq = new Rq("목록?searchKeyword=영광"); //key=value
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+        System.out.println(searchKeyword);
+
+        Rq rq2 = new Rq("목록?keywordType=content"); //key=value
+        String keywordType = rq2.getParam("keywordType", ""); // content
+        System.out.println(keywordType);
+
+        Rq rq3 = new Rq("목록?keywordType=author"); //key=value
+        String keywordType2 = rq3.getParam("keywordType", ""); // author
+        System.out.println(keywordType2);
+    }
+
+
 }
